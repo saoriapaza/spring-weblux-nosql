05867593eb2d0c68bb15f88e11ac4cc06558a98c
saoriapaza <saori.apaza.aldazabal@vallegrande.edu.pe>
2026-03-06 11:49:26 -0500

    started proyect

---
+++ src/test/java/ap1/_1/saori/apaza/SpringWebfluxNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package ap1._1.saori.apaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxNosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
